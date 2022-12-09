7856aaf31c0a5100691afb2999a18d695a7a2f55
tranmanhduc2711 <manhduc2711@gmail.com>
2022-12-10 02:04:41 +0700

    connect DB cloud

--- src/main/java/com/example/edubox/EduBoxApplication.java
+++ src/main/java/com/example/edubox/EduBoxApplication.java
@@ -2,8 +2,10 @@ package com.example.edubox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class EduBoxApplication {
 
     public static void main(String[] args) {
